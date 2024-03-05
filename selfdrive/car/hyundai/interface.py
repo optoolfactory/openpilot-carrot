43ecd16dacae7e741485c0132bd107acc639a11f
optoolfactory <80941254+optoolfactory@users.noreply.github.com>
2024-03-05 17:20:02 +0900

    Update interface.py
    gn7

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -205,7 +205,15 @@ class CarInterface(CarInterfaceBase):
       ret.mass = 2205.
       ret.wheelbase = 3.273
       ret.steerRatio = 11.94  # https://www.hyundai.com/content/dam/hyundai/au/en/models/staria-load/premium-pip-update-2023/spec-sheet/STARIA_Load_Spec-Table_March_2023_v3.1.pdf
-
+    elif candidate == CAR.GRANDEUR_GN7:
+      ret.mass = 1620.
+      ret.wheelbase = 2.895
+      ret.steerRatio = 14.2
+    elif candidate == CAR.GRANDEUR_GN7_HYBRID:
+      ret.mass = 1700.
+      ret.wheelbase = 2.895
+      ret.steerRatio = 14.2
+      
     # Kia
     elif candidate == CAR.KIA_SORENTO:
       ret.mass = 1985.
