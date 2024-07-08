a2a778b56787dd741020e325eea9fac54ecf3677
optoolfactory <80941254+optoolfactory@users.noreply.github.com>
2024-07-08 18:19:04 +0900

    Update values.py

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -356,7 +356,6 @@ class CAR(Platforms):
   HYUNDAI_CASPER = HyundaiPlatformConfig(
     [HyundaiCarDocs("Hyundai Casper 2023", "All", car_parts=CarParts.common([CarHarness.hyundai_k]))],
     CarSpecs(mass=1690, wheelbase=3.055, steerRatio=17),  # mass: from https://www.hyundai-motor.com.tw/clicktobuy/custin#spec_0, steerRatio: from learner
-    flags=HyundaiFlags.CHECKSUM_CRC8,
   )
   
 
