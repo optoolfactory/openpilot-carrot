0f03910e4dffdf9dab313017cacdc30bba72a5c4
optoolfactory <80941254+optoolfactory@users.noreply.github.com>
2024-10-12 02:28:10 +0900

    Update carcontroller.py

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -3,7 +3,7 @@ from openpilot.common.conversions import Conversions as CV
 from openpilot.common.numpy_fast import clip, interp
 from openpilot.common.realtime import DT_CTRL
 from opendbc.can.packer import CANPacker
-from openpilot.selfdrive.car import apply_driver_steer_torque_limits, common_fault_avoidance
+from openpilot.selfdrive.car import apply_driver_steer_torque_limits, apply_std_steer_angle_limits, common_fault_avoidance, make_tester_present_msg, structs
 from openpilot.selfdrive.car.hyundai import hyundaicanfd, hyundaican
 from openpilot.selfdrive.car.hyundai.hyundaicanfd import CanBus
 from openpilot.selfdrive.car.hyundai.values import HyundaiFlags, Buttons, CarControllerParams, CANFD_CAR, CAR, LEGACY_SAFETY_MODE_CAR, CAN_GEARS, HyundaiExtFlags
@@ -59,6 +59,9 @@ class CarController(CarControllerBase):
     self.apply_steer_last = 0
     self.car_fingerprint = CP.carFingerprint
     self.last_button_frame = 0
+    self.apply_angle_last = 0
+    self.lkas_max_torque = 0
+    self.driver_applied_torque_reducer = 0
     
     self.jerkStartLimit = 2.0
     self.speedCameraHapticEndFrame = 0
@@ -128,8 +131,56 @@ class CarController(CarControllerBase):
                                                                        self.angle_limit_counter, self.maxAngleFrames,
                                                                        MAX_ANGLE_CONSECUTIVE_FRAMES)
 
+    apply_angle = apply_std_steer_angle_limits(actuators.steeringAngleDeg, self.apply_angle_last, CS.out.vEgoRaw, self.params)
+
+    # Figure out torque value.  On Stock when LKAS is active, this is variable,
+    # but 0 when LKAS is not actively steering, so because we're "tricking" ADAS
+    # into thinking LKAS is always active, we need to make sure we're applying
+    # torque when the driver is not actively steering. The default value chosen
+    # here is based on observations of the stock LKAS system when it's engaged
+    # CS.out.steeringPressed and steeringTorque are based on the
+    # STEERING_COL_TORQUE value
+    MAX_TORQUE = 240
+    # Interpolate a percent to apply to max torque based on vEgo value, which is
+    # the "best estimate of speed".  This means that under 20 (units?) we will
+    # apply less torque, and over 20 we will apply the full calculated torque.
+    ego_weight = interp(CS.out.vEgo, [0, 5, 10, 20], [0.2, 0.3, 0.5, 1.0])
+
+    # Track if and how long the driver has been applying torque and create a
+    # value to reduce the max torque applied. This block will cause the
+    # `driver_applied_torque_reducer` to settle to value between 30 and 150.
+    # While the driver applies torque the value will decrease to 30, and while
+    # the driver is not applying torque the value will increase to 150.
+    if abs(CS.out.steeringTorque) > 200:
+      # If the driver is applying some torque manually, reduce the value down to 30 (the min)
+      self.driver_applied_torque_reducer -= 1
+      if self.driver_applied_torque_reducer < 30:
+        self.driver_applied_torque_reducer = 30
+    else:
+      # While the driver is not applying torque, increase the value up to 150 (the max)
+      self.driver_applied_torque_reducer += 1
+      if self.driver_applied_torque_reducer > 150:
+        self.driver_applied_torque_reducer = 150
+
+    if self.driver_applied_torque_reducer < 150:
+      # If the driver has just started applying torque, the reducer value will
+      # be around 150 so we won't reduce the max torque much. As the driver
+      # continues to apply torque, the reducer value will decrease to 30, so we
+      # will reduce the max torque more to fight them less (at this level we'll
+      # be doing 1/5 of the torque)
+      self.lkas_max_torque = int(round(MAX_TORQUE * ego_weight * (self.driver_applied_torque_reducer / 150)))
+    else:
+      # A torque reducer value of 150 means the driver has not been applying
+      # torque for a while, so we will apply the full max torque value, adjusted
+      # by the ego weight (based on driving speed)
+      self.lkas_max_torque = MAX_TORQUE * ego_weight
+
+
     if not CC.latActive:
+      apply_angle = CS.out.steeringAngleDeg
       apply_steer = 0
+      self.lkas_max_torque = 0
+
 
     # Hold torque with induced temporary fault when cutting the actuation bit
     torque_fault = CC.latActive and not apply_steer_req
@@ -206,7 +257,10 @@ class CarController(CarControllerBase):
       hda2_long = hda2 and self.CP.openpilotLongitudinalControl
 
       # steering control
-      can_sends.extend(hyundaicanfd.create_steering_messages(self.packer, self.CP, self.CAN, CC.enabled, apply_steer_req, apply_steer))
+      can_sends.extend(hyundaicanfd.create_steering_messages(self.packer, self.CP, self.CAN, CC.enabled,
+                                                             apply_steer_req, CS.out.steeringPressed,
+                                                             apply_steer, apply_angle, self.lkas_max_torque))
+
 
       ## carrot 기존데이터를 복사하니.. mdps에러가 뜨는것 같음...
       #if self.CP.extFlags & HyundaiExtFlags.SCC_BUS2.value:  
@@ -225,7 +279,8 @@ class CarController(CarControllerBase):
                                                             self.CP.flags & HyundaiFlags.CANFD_HDA2_ALT_STEERING))
 
       # LFA and HDA icons
-      if self.frame % 5 == 0 and (not hda2 or hda2_long):
+      updateLfaHdaIcons = (not hda2 or hda2_long) or self.CP.carFingerprint in (CAR.HYUNDAI_SANTA_FE_2024_MX5, CAR.HYUNDAI_SANTA_FE_2024_HEV_MX5)
+      if self.frame % 5 == 0 and updateLfaHdaIcons:
         can_sends.append(hyundaicanfd.create_lfahda_cluster(self.packer, self.CAN, CC.enabled))
 
       # blinkers
@@ -314,6 +369,7 @@ class CarController(CarControllerBase):
     new_actuators = actuators.copy()
     new_actuators.steer = apply_steer / self.cc_params.STEER_MAX
     new_actuators.steerOutputCan = apply_steer
+    new_actuators.steeringAngleDeg = apply_angle
     new_actuators.accel = accel
 
     self.frame += 1
