fdd09000ca15f4c04ecd76450beda20a41e26a54
optoolfactory <80941254+optoolfactory@users.noreply.github.com>
2024-10-12 02:30:29 +0900

    Update carstate.py

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -350,7 +350,7 @@ class CarState(CarStateBase):
       cp_ = cp_cam if (self.CP.extFlags & HyundaiExtFlags.SCC_BUS2 and self.CP.extFlags & HyundaiExtFlags.BSM_IN_ADAS.value) else cp
       #ret.leftBlindspot = cp.vl["BLINDSPOTS_REAR_CORNERS"]["FL_INDICATOR"] != 0
       #ret.rightBlindspot = cp.vl["BLINDSPOTS_REAR_CORNERS"]["FR_INDICATOR"] != 0
-      if self.CP.carFingerprint in [CAR.KIA_CARNIVAL_4TH_GEN]:
+      if self.CP.carFingerprint in [CAR.KIA_CARNIVAL_4TH_GEN, CAR.HYUNDAI_SANTA_FE_2024_MX5, CAR.HYUNDAI_SANTA_FE_2024_HEV_MX5]:
         ret.leftBlindspot = cp_.vl["BLINDSPOTS_REAR_CORNERS"]["INDICATOR_LEFT_FOUR"] != 0
         ret.rightBlindspot = cp_.vl["BLINDSPOTS_REAR_CORNERS"]["INDICATOR_RIGHT_FOUR"] != 0
       else:
