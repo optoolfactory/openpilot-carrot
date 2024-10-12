e370fce173ad549fcf9e976c73a54ae41271b253
ajouatom <ajouatom@gmail.com>
2024-10-12 16:50:07 +0900

    fix...

--- selfdrive/carrot/carrot_man.py
+++ selfdrive/carrot/carrot_man.py
@@ -1184,7 +1184,7 @@ class CarrotServ:
     if "carrotIndex" in json:
       self.carrotIndex = int(json.get("carrotIndex"))
 
-    if self.carrotIndex % 10 == 0 and "epochTime" in json:
+    if self.carrotIndex % 60 == 0 and "epochTime" in json:
       # op는 ntp를 사용하기때문에... 필요없는 루틴으로 보임.
       timezone_remote = json.get("timezone", "Asia/Seoul")
       
