ef904c8ff243904b78d2547d311fffe698c9d42e
optoolfactory <80941254+optoolfactory@users.noreply.github.com>
2024-10-22 19:19:53 +0900

    Update values.py

--- opendbc_repo/opendbc/car/hyundai/values.py
+++ opendbc_repo/opendbc/car/hyundai/values.py
@@ -144,6 +144,9 @@ class HyundaiPlatformConfig(PlatformConfig):
     if self.flags & HyundaiFlags.MIN_STEER_32_MPH:
       self.specs = self.specs.override(minSteerSpeed=32 * CV.MPH_TO_MS)
 
+    if self.flags & HyundaiFlags.CAN_CANFD_HYBRID:
+      self.dbc_dict = dbc_dict('hyundai_palisade_2023_generated', None)
+
 
 @dataclass
 class HyundaiCanFDPlatformConfig(PlatformConfig):
