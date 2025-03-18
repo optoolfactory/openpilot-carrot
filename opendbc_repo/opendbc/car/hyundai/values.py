d4d377a96d77dcd5593516481b77cc00df9319df
optoolfactory <80941254+optoolfactory@users.noreply.github.com>
2025-03-18 13:56:03 +0900

    Update values.py

--- opendbc_repo/opendbc/car/hyundai/values.py
+++ opendbc_repo/opendbc/car/hyundai/values.py
@@ -411,7 +411,7 @@ class CAR(Platforms):
       HyundaiCarDocs("Hyundai SANTAFE HYBRID (MX5)", car_parts=CarParts.common([CarHarness.hyundai_k])),
     ],
     HYUNDAI_SANTAFE_MX5.specs,
-    flags=HyundaiFlags.ANGLE_CONTROL,
+    flags=HyundaiFlags.ANGLE_CONTROL | HyundaiFlags.HYBRID, 
   )  
   GRANDEUR_GN7 = HyundaiCanFDPlatformConfig(
     [HyundaiCarDocs("Hyundai GRANDEUR_GN7 2023", "All", car_parts=CarParts.common([CarHarness.hyundai_m]))],
@@ -423,6 +423,14 @@ class CAR(Platforms):
     CarSpecs(mass=1700, wheelbase=2.895, steerRatio=14.2),
     flags=HyundaiFlags.HYBRID | HyundaiFlags.RADAR_SCC,
   )
+  HYUNDAI_PALISADE_LX3 = HyundaiCanFDPlatformConfig(
+    [
+      HyundaiCarDocs("Hyundai Palisade 2025 (LX3)", "All", video_link="https://youtu.be/TAnDqjF4fDY?t=456", car_parts=CarParts.common([CarHarness.hyundai_h])),
+    ],
+    CarSpecs(mass=2020, wheelbase=2.97, steerRatio=15.6 * 1.15, tireStiffnessFactor=0.63),
+    flags=HyundaiFlags.ANGLE_CONTROL,
+  )
+
 
 
 
@@ -772,13 +780,6 @@ class CAR(Platforms):
     ],
     CarSpecs(mass=1835, wheelbase=2.68, steerRatio=14.02),
     flags=HyundaiFlags.EV | HyundaiFlags.ANGLE_CONTROL,
-  )
-  HYUNDAI_PALISADE_LX3 = HyundaiCanFDPlatformConfig(
-    [
-      HyundaiCarDocs("Hyundai Palisade 2025 (LX3)", "All", video_link="https://youtu.be/TAnDqjF4fDY?t=456", car_parts=CarParts.common([CarHarness.hyundai_h])),
-    ],
-    CarSpecs(mass=2020, wheelbase=2.97, steerRatio=15.6 * 1.15, tireStiffnessFactor=0.63),
-    flags=HyundaiFlags.ANGLE_CONTROL,
   )
 
 class Buttons:
