9957f3878800caa9177238c7a3c56c23ccacef1c
optoolfactory <80941254+optoolfactory@users.noreply.github.com>
2025-04-01 01:29:19 +0900

    Update manager.py

--- system/manager/manager.py
+++ system/manager/manager.py
@@ -149,7 +149,7 @@ def get_default_params():
     ("CustomSteerDeltaUp", "0"),
     ("CustomSteerDeltaDown", "0"),
     ("SpeedFromPCM", "1"),
-    ("SteerActuatorDelay", "30"),
+    ("SteerActuatorDelay", "20"),
     ("MaxTimeOffroadMin", "60"),
     ("DisableDM", "1"),
     ("RecordRoadCam", "0"),
