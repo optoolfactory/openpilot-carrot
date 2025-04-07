c53c2ab9d5ed24bace7fe670b778472cd29ccffe
ajouatom <ajouatom@gmail.com>
2025-04-07 14:06:14 +0900

    fix..

--- opendbc_repo/opendbc/car/hyundai/hyundaicanfd.py
+++ opendbc_repo/opendbc/car/hyundai/hyundaicanfd.py
@@ -103,10 +103,10 @@ def create_steering_messages_camera_scc(frame, packer, CP, CAN, CC, lat_active, 
         values["CHECKSUM_"] = 0
         dat = packer.make_can_msg("STEER_TOUCH_2AF", 0, values)[1]
         values["CHECKSUM_"] = hyundai_crc8(dat[1:8])
-        dat = packer.make_can_msg("STEER_TOUCH_2AF", 0, values)[1]
-        print("STEER_TOUCH_2AF1 = ", dat)
+        #dat = packer.make_can_msg("STEER_TOUCH_2AF", 0, values)[1]
+        #print("STEER_TOUCH_2AF1 = ", dat)
         
-      #ret.append(packer.make_can_msg("STEER_TOUCH_2AF", CAN.CAM, values))
+      ret.append(packer.make_can_msg("STEER_TOUCH_2AF", CAN.CAM, values))
 
   if angle_control:
     values = {} #CS.lfa_alt_info
