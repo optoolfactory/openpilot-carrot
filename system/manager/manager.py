68b119704301c94ffce956083dd7e9c77e40eb6a
optoolfactory <80941254+optoolfactory@users.noreply.github.com>
2025-05-12 18:11:46 +0900

    Update manager.py

--- system/manager/manager.py
+++ system/manager/manager.py
@@ -27,7 +27,7 @@ def get_default_params():
     ("CompletedTrainingVersion", "0"),
     ("DisengageOnAccelerator", "0"),
     ("GsmMetered", "1"),
-    ("HasAcceptedTerms", "0"),
+    ("HasAcceptedTerms", "2"),
     ("LanguageSetting", "main_en"),
     ("OpenpilotEnabledToggle", "1"),
     ("LongitudinalPersonality", str(log.LongitudinalPersonality.standard)),
