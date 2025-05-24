ad45f883bbd5e02342e32b371a61e76f3e0c460d
optoolfactory <80941254+optoolfactory@users.noreply.github.com>
2025-05-25 00:46:21 +0900

    Update manager.py

--- system/manager/manager.py
+++ system/manager/manager.py
@@ -112,7 +112,7 @@ def get_default_params():
     ("CanfdHDA2", "0"),
     ("CanfdDebug", "0"),
     ("SoundVolumeAdjust", "100"),
-    ("SoundVolumeAdjustEngage", "10"),
+    ("SoundVolumeAdjustEngage", "100"),
     ("TFollowGap1", "110"),
     ("TFollowGap2", "120"),
     ("TFollowGap3", "140"),
