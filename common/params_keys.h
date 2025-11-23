fc82a03b154bf368d3dfe6d76a8d09912b8be838
optoolfactory <80941254+optoolfactory@users.noreply.github.com>
2025-11-23 23:03:59 +0900

    Update params_keys.h
    c3x lite

--- common/params_keys.h
+++ common/params_keys.h
@@ -287,4 +287,5 @@ inline static std::unordered_map<std::string, uint32_t> keys = {
     {"NNFF", PERSISTENT},
     {"NNFFLite", PERSISTENT},
     {"NNFFModelName", CLEAR_ON_OFFROAD_TRANSITION},
+    {"HardwareC3xLite", PERSISTENT},
 };
