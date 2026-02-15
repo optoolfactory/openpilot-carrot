d50c72bb7875733ee7b9cf7d7c024df7525cead2
ajouatom <ajouatom@gmail.com>
2026-02-16 08:21:26 +0900

    fix..

--- opendbc_repo/opendbc/car/interfaces.py
+++ opendbc_repo/opendbc/car/interfaces.py
@@ -299,12 +299,11 @@ class CarStateBase(ABC):
     cs.vEgoRaw = sum((fl, fr, rl, rr)) / 4 * unit * self.CP.wheelSpeedFactor
     cs.vEgo, cs.aEgo = self.update_speed_kf(cs.vEgoRaw)
 
-  
-  def update_clu_speed_kf(self, v_ego_raw):
-    if abs(v_ego_raw - self.v_ego_clu_kf.x[0][0]) > 2.0:  # Prevent large accelerations when car starts at non zero speed
-      self.v_ego_clu_kf.set_x([[v_ego_raw], [0.0]])
+  def update_speed_kf(self, v_ego_raw):
+    if abs(v_ego_raw - self.v_ego_kf.x[0][0]) > 2.0:  # Prevent large accelerations when car starts at non zero speed
+      self.v_ego_kf.set_x([[v_ego_raw], [0.0]])
 
-    v_ego_x = self.v_ego_clu_kf.update(v_ego_raw)
+    v_ego_x = self.v_ego_kf.update(v_ego_raw)
     return float(v_ego_x[0]), float(v_ego_x[1])
 
   def update_blinker_from_lamp(self, blinker_time: int, left_blinker_lamp: bool, right_blinker_lamp: bool):
