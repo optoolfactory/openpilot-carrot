fce0603419b170756a5e5e793a42274828ecf771
ajouatom <ajouatom@gmail.com>
2026-02-16 12:44:34 +0900

    test

--- opendbc_repo/opendbc/carrot/hyundai/carrot_carstate.py
+++ opendbc_repo/opendbc/carrot/hyundai/carrot_carstate.py
@@ -125,12 +125,11 @@ class CarrotCarState(CarStateBase):
     elif self.gear_msg_canfd == "GEAR":
       paddle_button = 1 if self.cp.vl["GEAR"]["LEFT_PADDLE"] == 1 else 2 if self.cp.vl["GEAR"]["RIGHT_PADDLE"] == 1 else 0
 
-    for e in create_button_events(
-      paddle_button,
-      self.paddle_button_prev,
-      {1: ButtonType.paddleLeft, 2: ButtonType.paddleRight}
-    ):
-      ret.buttonEvents.append(e)
+    ret.buttonEvents = [
+      *list(ret.buttonEvents),
+      *create_button_events(paddle_button, self.paddle_button_prev,
+                            {1: ButtonType.paddleLeft, 2: ButtonType.paddleRight}),
+    ]
     self.paddle_button_prev = paddle_button
   
     return ret
