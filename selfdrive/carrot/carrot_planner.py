707dcb8acc667496ef37409a31f482a627060cf0
ajouatom <ajouatom@gmail.com>
2026-02-17 21:39:34 +0900

    fix..

--- selfdrive/carrot/carrot_planner.py
+++ selfdrive/carrot/carrot_planner.py
@@ -5,11 +5,43 @@ from openpilot.common.params import Params
 import numpy as np
 from openpilot.common.realtime import DT_MDL
 from openpilot.common.constants import CV
-from openpilot.common.filter_simple import MyMovingAverage
+from openpilot.common.utils import MovingAverage
 from openpilot.selfdrive.selfdrived.events import Events
+from collections import deque
 
 EventName = log.OnroadEvent.EventName
 LaneChangeState = log.LaneChangeState
+
+class MyMovingAverage:
+  def __init__(self, window_size, value=None):
+    self.window_size = window_size
+    if (value is not None):
+      self.values = deque([value] * window_size, maxlen=window_size)
+      self.sum = value * window_size
+      self.result = value
+    else:
+      self.values = deque(maxlen=window_size)
+      self.sum = 0
+      self.result = 0
+
+  def set(self, value):
+    self.values.clear()
+    self.values.append(value)
+    self.sum = value
+    self.result = value
+    return value
+
+  def set_all(self, value):
+    self.values = deque([value] * self.window_size, maxlen=self.window_size)
+    self.sum = value * self.window_size
+    self.result = value
+    return value
+
+  def process(self, value, median=False):
+    self.values.append(value)
+    self.sum = sum(self.values)  
+    self.result = float(np.median(self.values)) if median else float(self.sum) / len(self.values)
+    return self.result
 
 class XState(Enum):
   lead = 0
