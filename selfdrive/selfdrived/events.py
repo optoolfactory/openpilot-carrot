5298d08972a078e8bd84daaa0643d7d861756832
ajouatom <ajouatom@gmail.com>
2026-02-17 18:12:50 +0900

    fix..

--- selfdrive/selfdrived/events.py
+++ selfdrive/selfdrived/events.py
@@ -379,7 +379,7 @@ def personality_changed_alert(CP: car.CarParams, CS: car.CarState, sm: messaging
 
 def carstate_log_alert(CP: car.CarParams, CS: car.CarState, sm: messaging.SubMaster, metric: bool, soft_disable_time: int, personality) -> Alert:
   vals = f"CS:{CS.logCarrot}"
-  return Alert(vals, "", AlertStatus.userPrompt, AlertSize.small, Priority.HIGH, VisualAlert.none, AudibleAlert.none, 0.2)
+  return Alert(vals, "", AlertStatus.normal, AlertSize.small, Priority.HIGHEST, VisualAlert.none, AudibleAlert.none, 0.5)
 
 
 def invalid_lkas_setting_alert(CP: car.CarParams, CS: car.CarState, sm: messaging.SubMaster, metric: bool, soft_disable_time: int, personality) -> Alert:
@@ -1035,7 +1035,7 @@ EVENTS: dict[int, dict[str, Alert | AlertCallbackType]] = {
       Priority.LOW, VisualAlert.none, AudibleAlert.none, .1),
   },
   EventName.carStateLog: {
-    ET.WARNING: carstate_log_alert,
+    ET.PERMANENT: carstate_log_alert,
   },
 }
 
