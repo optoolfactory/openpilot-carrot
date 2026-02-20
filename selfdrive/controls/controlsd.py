c7cacd8fda814732c6db176aa56c61c52578c19e
ajouatom <ajouatom@gmail.com>
2026-02-20 16:08:10 +0900

    revert

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -328,8 +328,7 @@ class Controls:
       self.update()
       CC, lac_log = self.state_control()
       self.publish(CC, lac_log)
-      #rk.monitor_time()
-      rk.keep_time()
+      rk.monitor_time()
 
 
 def main():
