36a0a5cc317f481d2efc4480509dff379d5317f6
optoolfactory <80941254+optoolfactory@users.noreply.github.com>
2026-02-23 01:41:05 +0900

    Update params_keys.h

--- common/params_keys.h
+++ common/params_keys.h
@@ -298,7 +298,7 @@ inline static std::unordered_map<std::string, ParamKeyAttributes> keys = {
     {"SpeedFromPCM", {PERSISTENT, INT, "1"}},
     {"MaxTimeOffroadMin", {PERSISTENT, INT, "60"}},
 
-    {"DisableDM", {PERSISTENT, INT, "2"}},
+    {"DisableDM", {PERSISTENT, INT, "0"}},
     {"EnableConnect", {PERSISTENT, INT, "0"}},
     {"MuteDoor", {PERSISTENT, INT, "1"}},
     {"MuteSeatbelt", {PERSISTENT, INT, "1"}},
