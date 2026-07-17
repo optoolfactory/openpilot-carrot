d930cc3921538fb477214321d90910909c5209f1
optoolfactory <80941254+optoolfactory@users.noreply.github.com>
2026-07-17 16:32:11 +0900

    Update selfdrived.py

--- openpilot/selfdrive/selfdrived/selfdrived.py
+++ openpilot/selfdrive/selfdrived/selfdrived.py
@@ -128,6 +128,7 @@ class SelfdriveD:
     self.personality = self.read_personality_param()
     self.recalibrating_seen = False
     self.dm_lockout_set = False
+    self.cutin_audio_active = False
     self.dm_uncertain_alerted = False
     self.state_machine = StateMachine()
     self.rk = Ratekeeper(100, print_delay_threshold=None)
@@ -192,6 +193,11 @@ class SelfdriveD:
     if self.CP.passive:
       return
 
+    cutin_active = self.enabled and self.sm.valid['radarState'] and len(self.sm['radarState'].leadsCutIn) > 0
+    if cutin_active and not self.cutin_audio_active:
+      self.events.add(EventName.audioPrompt)
+    self.cutin_audio_active = cutin_active
+    
     # Block resume if cruise never previously enabled
     resume_pressed = any(be.type in (ButtonType.accelCruise, ButtonType.resumeCruise) for be in CS.buttonEvents)
     if not self.CP.pcmCruise and CS.vCruise > 250 and resume_pressed:
