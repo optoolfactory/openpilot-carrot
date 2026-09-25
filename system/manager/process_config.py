1786fb0b8a1d619e127ef70cf1bfe67ca432f348
optoolfactory <80941254+optoolfactory@users.noreply.github.com>
2026-09-25 19:34:58 +0900

    Update process_config.py

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -108,7 +108,7 @@ def enable_cluster_hud(started, params, CP: car.CarParams) -> bool:
 procs = [
   DaemonProcess("manage_athenad", "system.athena.manage_athenad", "AthenadPid"),
 
-  #NativeProcess("loggerd", "system/loggerd", ["./loggerd"], logging),
+  NativeProcess("loggerd", "system/loggerd", ["./loggerd"], logging),
   NativeProcess("encoderd", "system/loggerd", ["./encoderd"], only_onroad),
   NativeProcess("stream_encoderd", "system/loggerd", ["./encoderd", "--stream"], or_(notcar, and_(only_onroad, or_(enable_webrtc, enable_youtube_hq)))),
   NativeProcess("youtube_encoderd", "system/loggerd", ["./encoderd", "--youtube"], and_(only_onroad, enable_youtube_encoder)),
